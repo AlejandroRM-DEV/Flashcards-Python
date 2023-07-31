bfdb83a4202899d31b2f38e505a6b008d4cfba36
Alejandro RM <alejandror45@gmail.com>
2023-07-31 09:59:31 -0600

    Stage 3/7: Make it your own   Practice arrays and loops: create a new card for the program to play with you.

--- Flashcards (Python)/task/flashcards/flashcards.py
+++ Flashcards (Python)/task/flashcards/flashcards.py
@@ -1,5 +1,17 @@
-# Write your code here
-print("Card:")
-print("purchase")
-print("Definition:")
-print("buy")
+print("Input the number of cards:")
+number_of_cards = int(input())
+cards = []
+for n in range(1, number_of_cards + 1):
+    print(f"The term for card #{n}:")
+    term = input()
+    print(f"The definition for card #{n}:")
+    definition = input()
+    cards.append({"term": term, "definition": definition})
+
+for card in cards:
+    print(f"Print the definition of \"{card['term']}\":")
+    answer = input()
+    if answer == card["definition"]:
+        print("Correct!")
+    else:
+        print(f"Wrong. The right answer is {card['definition']}.")
